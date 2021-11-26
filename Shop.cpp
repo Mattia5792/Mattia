2a1c53ad6a77a1bd0a8a0107a4e5ae742aa6eece
Mattia5792 <93124269+Mattia5792@users.noreply.github.com>
2021-11-26 11:50:31 +0100

    Add files via upload

---
+++ Shop.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	double costo;
+	string codice_prodotto;
+	int quantita;
+	cout<<"---------------------------------------------------------------------------"<<endl;
+	cout<<"--                             RAPISARDI SHOP                            --"<<endl;
+	cout<<"---------------------------------------------------------------------------"<<endl<<endl;
+	
+	cout<<"-----------------------------------------------------"<<endl;
+	cout<<"--               ELENCO DEI PRODOTTI               --"<<endl;
+	cout<<"-----------------------------------------------------"<<endl;
+	cout<<"Mele 2 euro/kg\n";
+	cout<<"Pere 3 euro/kg\n";
+	cout<<"Arance 4 euro/kg\n";
+	cout<<"Sapone per piatti 5 euro\n";
+	cout<<"Amuchina 2 euro\n";
+	cout<<"Bresaola 15 euro/kg\n";
+	cout<<"Prociutto cotto gran biscotto 20 euro/kg\n";
+	system ("PAUSE");
+}
+
